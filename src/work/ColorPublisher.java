e66bcd1605e2ee5ecb57fdff78e37caf0bfb62e1
chenjiam <chenjiaming@eastpharm.com>
2024-04-12 16:51:38 +0800

    work 2

---
+++ src/work/ColorPublisher.java
@@ -0,0 +1,15 @@
+package work;// TODO: Add code to this file as necessary
+//       to finish the implementation of ColorPublisher.
+
+import java.awt.Color;
+
+public class ColorPublisher
+{
+    public void addSubscriber(ColorSubscriber cs)
+    {
+    }
+    public void publish(Color c)
+    {
+
+    }
+}
---
+++ src/work/ColorSubscriber.java
@@ -0,0 +1,8 @@
+package work;
+
+import java.awt.Color;
+
+public interface ColorSubscriber
+{
+    public void notifyColorChange(Color c);
+}
