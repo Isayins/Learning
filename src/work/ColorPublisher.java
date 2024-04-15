347bbae9c44602de9f6624039864798314d7d9f6
chenjiam <chenjiaming@eastpharm.com>
2024-04-15 16:51:04 +0800

    实现按钮功能

--- src/work/ColorPublisher.java
+++ src/work/ColorPublisher.java
@@ -2,14 +2,22 @@ package work;// TODO: Add code to this file as necessary
 //       to finish the implementation of ColorPublisher.
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class ColorPublisher
 {
+    private ArrayList<ColorSubscriber> subscribers = new ArrayList<>();
     public void addSubscriber(ColorSubscriber cs)
     {
+        subscribers.add(cs);
+
     }
     public void publish(Color c)
     {
+        // 通知所有的订阅者颜色发生了变化
 
+        for (ColorSubscriber subscriber : subscribers) {
+            subscriber.notifyColorChange(c);
+        }
     }
 }
