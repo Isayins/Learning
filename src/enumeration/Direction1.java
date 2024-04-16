70436540cc95590958ec283844ccacaf9c212556
chenjiam <chenjiaming@eastpharm.com>
2024-04-16 16:48:08 +0800

    测试提交3

---
+++ src/enumeration/Direction1.java
@@ -0,0 +1,12 @@
+package enumeration;
+
+public class Direction1 {
+    // 创建几个实例
+    public static final Direction1 FRONT = new Direction1();
+    public static final Direction1 BEHIND = new Direction1();
+    public static final Direction1 LEFT = new Direction1();
+    public static final Direction1 RIGHT = new Direction1();
+
+    // 私有构造方法
+    private Direction1() {}
+}
